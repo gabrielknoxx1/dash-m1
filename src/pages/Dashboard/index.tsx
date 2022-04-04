aa8827d95845158e4f597eb1d9f257239fbd70d3
Gabriel Sousa <ggs@lionx.com.br>
2022-04-04 10:54:36 -0300

    update

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,11 +6,10 @@ interface DashboardProps {
 }
 
 export const Dashboard = ({ token }: DashboardProps) => {
+  if (!token || token !== "5f768b56-8662-4071-88c4-f728a9a950de") return;
   return (
-    <RootProvider token={token}>
-      <Container>
-        <h1>Dashboard - M1</h1>
-      </Container>
-    </RootProvider>
+    <Container>
+      <h1>Dashboard - M1</h1>
+    </Container>
   );
 };
